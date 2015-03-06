1c2be0e8f91d221d33ea01f68e84aa5fa50d0b78
Alexander Price <alexander@Alexanders-MacBook-Pro.local>
2015-03-05 19:57:38 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var express      = require('express')
+var cookieParser = require('cookie-parser')
+ 
+var app = express()
+app.use(cookieParser())
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cokkieName;
+  if (cookie === undefined)
+  {
+    // no: set a new cookie
+    var randomNumber=Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cokkieName',randomNumber, { maxAge: 900000, httpOnly: true });
+  }
+
+  next(); // <-- important!
+});
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.get('/test', function(req, res){
+  getSocket();
+  res.send();
+});
+
+io.on('connection', function(socket){
+  console.log('connected');
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+function getSocket() {
+  for(var i in io.sockets.connected) {
+    var socket = io.sockets.connected[i].request.headers.cookie;
+    console.log(socket);//result is : io=pYb2vzAd8wWWKb......
+  }
+}
